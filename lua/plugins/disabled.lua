92ae605bfa0b495881e98164a0178f16895ed48c
ilnb <lnbsa12345@gmail.com>
2024-12-13 13:44:00 +0530

    disabled some plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+
+  {
+    "MagicDuck/grug-far.nvim",
+    enabled = false,
+  },
+
+  {
+    "nvim-lua/plenary.nvim",
+    enabled = false,
+  },
+
+  {
+    "folke/ts-comments.nvim",
+    enabled = false,
+  },
+}
