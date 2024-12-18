6172cd07f0099e0d924ea7e529e363997b8111d6
ilnb <lnbsa12345@gmail.com>
2024-12-18 15:52:06 +0530

    minor changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,23 @@
 return {
+  -- default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
 
   -- catppuccin
   {
     "catppuccin/nvim",
     enabled = false,
+  },
+
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
   },
 
   -- kanagawa
@@ -13,17 +27,17 @@ return {
     priority = 1000,
     config = function()
       require("kanagawa").setup({
-        compile = true, -- enable compiling the colorscheme
+        compile = true,   -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
         functionStyle = {},
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
+        colors = {             -- add/modify theme and palette colors
           palette = {},
           theme = {
             wave = {
@@ -37,13 +51,12 @@ return {
         overrides = function(colors) -- add/modify highlights
           return {}
         end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
           dark = "wave", -- try "dragon" !
           light = "lotus",
         },
       })
-      vim.cmd([[colorscheme kanagawa]])
     end,
   },
 
@@ -54,13 +67,13 @@ return {
     config = function()
       require("material").setup({
         contrast = {
-          terminal = false, -- Enable contrast for the built-in terminal
-          sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-          floating_windows = false, -- Enable contrast for floating windows
-          cursor_line = false, -- Enable darker background for the cursor line
-          lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
+          terminal = false,            -- Enable contrast for the built-in terminal
+          sidebars = false,            -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+          floating_windows = false,    -- Enable contrast for floating windows
+          cursor_line = false,         -- Enable darker background for the cursor line
+          lsp_virtual_text = false,    -- Enable contrasted background for lsp virtual text
           non_current_windows = false, -- Enable contrasted background for non-current windows
-          filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+          filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
         },
 
         styles = { -- Give comments style such as bold, italic, underline etc.
@@ -112,25 +125,26 @@ return {
 
         disable = {
           colored_cursor = false, -- Disable the colored cursor
-          borders = false, -- Disable borders between vertically split windows
-          background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-          term_colors = false, -- Prevent the theme from setting terminal colors
-          eob_lines = false, -- Hide the end-of-buffer lines
+          borders = false,        -- Disable borders between vertically split windows
+          background = false,     -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+          term_colors = false,    -- Prevent the theme from setting terminal colors
+          eob_lines = false,      -- Hide the end-of-buffer lines
         },
 
         high_visibility = {
           lighter = false, -- Enable higher contrast text for lighter style
-          darker = false, -- Enable higher contrast text for darker style
+          darker = false,  -- Enable higher contrast text for darker style
         },
 
         lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
 
-        async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
+        async_loading = true,      -- Load parts of the theme asynchronously for faster startup (turned on by default)
 
-        custom_colors = nil, -- If you want to override the default colors, set this to a function
+        custom_colors = nil,       -- If you want to override the default colors, set this to a function
 
-        custom_highlights = {}, -- Overwrite highlights with your own
+        custom_highlights = {},    -- Overwrite highlights with your own
       })
+      vim.g.material_style = "deep ocean"
     end,
   },
 }
