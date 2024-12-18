799fa3c702ef3e83a9929985422a4cc62ffe9d65
ilnb <lnbsa12345@gmail.com>
2024-12-18 22:14:30 +0530

    minor changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,14 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    lazy = true,
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-          },
+  "nvim-neo-tree/neo-tree.nvim",
+  lazy = true,
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
         },
-      })
-    end,
-  },
+      },
+    })
+  end,
 }
