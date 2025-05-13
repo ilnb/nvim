9b1cac9ed678f25b78d2bbc3aea054d00c74d762
lnb <lnbsa12345@gmail.com>
2025-05-13 22:55:37 +0530

    minor update

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -178,7 +178,7 @@ return {
             action = function()
               require("fzf-lua").files({
                 cwd = "~/raylib/",
-                -- fd_opts = "--no-ignore --type f --exclude .git --hidden",
+                -- fd_opts = "--no-ignore --type file --exclude .git --hidden",
                 fd_opts = "-I -t f -E .git -H",
                 actions = {
                   ["default"] = function(selected, opts)
