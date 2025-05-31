ecdf87fb5fdb7eb96c90fe42124a3f2c831e4bf3
lnb <lnbsa12345@gmail.com>
2025-05-31 19:47:37 +0530

    reverted to lazyvim's <leader>ff

--- lua/plugin-cfg/fzf.lua
+++ lua/plugin-cfg/fzf.lua
@@ -27,27 +27,6 @@ M.keys = {
     desc = "Zsh config",
     mode = { "n", "v" },
   },
-  {
-    "<leader><leader>",
-    function()
-      local buf_path = vim.api.nvim_buf_get_name(0)
-      local file_dir = buf_path ~= "" and vim.fn.fnamemodify(buf_path, ":h") or nil
-
-      local base_dir = file_dir or vim.fn.getcwd()
-      local git_cmd = "git -C '" .. base_dir .. "' rev-parse --show-toplevel"
-
-      local git_root = vim.fn.systemlist(git_cmd)[1]
-      if not git_root or git_root == "" or git_root:match("^fatal") then
-        git_root = base_dir
-      end
-
-      require("fzf-lua").files({
-        cwd = vim.fn.fnamemodify(git_root, ":p"),
-        fd_opts = "-I -t f -E .git -H",
-      })
-    end,
-    desc = "Find Files (git-files or cwd)",
-  },
   { "<leader>fF", false },
 }
 
