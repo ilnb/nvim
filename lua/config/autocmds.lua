0dcb8d66e0831ae50124e8fbe0cab03f591eadd4
lnb <lnbsa12345@gmail.com>
2025-06-08 01:58:05 +0530

    autocmd to indent assembly

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- asm files indentation
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { '*.asm', '*.s', '*.S' },
+  callback = function()
+    vim.cmd [[norm! gg=G]]
+  end
+})
