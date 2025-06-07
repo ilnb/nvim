6e756055bac4f84c3c943b8289399ef8530d3fdc
lnb <lnbsa12345@gmail.com>
2025-06-07 15:32:43 +0530

    vscode snippets are garbage

--- lua/plugin-cfg/lspconfig.lua
+++ lua/plugin-cfg/lspconfig.lua
@@ -1,13 +1,31 @@
 local M = {}
 
+local capabilities = require('blink.cmp').get_lsp_capabilities({
+  textDocument = {
+    completion = {
+      completionItem =
+      {
+        snippetSupport = false,
+      },
+    },
+  },
+})
+
 ---@param opts PluginLspOpts
 M.opts = function(_, opts)
-  local asm = {
+  local asm_lsp = {
     cmd = { 'asm-lsp' },
     filetypes = { 'asm', 's' },
-    root_dir = function() return vim.fn.expand("~") end
+    root_dir = function() return vim.fn.expand("~") end,
   }
-  opts.servers["asm_lsp"] = asm
+  opts.servers["asm_lsp"] = asm_lsp
+  opts.servers["clangd"].capabilities = vim.tbl_deep_extend("force", opts.servers["clangd"].capabilities, capabilities)
+
+  -- local servers = { "asm_lsp", "clangd", "lua_ls", "pyright", "zls" }
+  -- for _, lsp in ipairs(servers) do
+  --   opts.servers[lsp] = opts.servers[lsp] or {}
+  --   opts.servers[lsp].capabilities = vim.tbl_deep_extend("force", opts.servers[lsp].capabilities or {}, capabilities)
+  -- end
 end
 
 return M
