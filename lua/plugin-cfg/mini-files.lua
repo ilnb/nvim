07d423181c0153f02736072b6e2f21fbdd6730ad
lnb <lnbsa12345@gmail.com>
2025-06-16 20:33:01 +0530

    config autoruns

--- lua/plugin-cfg/mini-files.lua
+++ lua/plugin-cfg/mini-files.lua
@@ -1,16 +1,12 @@
-local M  = {}
+local M = {}
 
-M.keys   = function(_, keys)
+M.keys  = function(_, keys)
   return keys
 end
 
-M.opts   = function(_, opts)
+M.opts  = function(_, opts)
   opts.windows.max_number = 4
   return opts
-end
-
-M.config = function(_, opts)
-  require('lazyvim.plugins.extras.editor.mini-files').config(_, opts)
 end
 
 return M
