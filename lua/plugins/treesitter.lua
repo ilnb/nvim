c155a2cfc6ce8e1c716a15a22b1ee82751b8b8cd
lnb <lnbsa12345@gmail.com>
2025-06-28 23:26:02 +0530

    minor cleanup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -94,12 +94,12 @@ return {
       local move = require 'nvim-treesitter.textobjects.move' ---@type table<string,fun(...)>
       local configs = require 'nvim-treesitter.configs'
       for name, fn in pairs(move) do
-        if name:find('goto') == 1 then
+        if name:find 'goto' == 1 then
           move[name] = function(q, ...)
             if vim.wo.diff then
               local config = configs.get_module 'textobjects.move'[name] ---@type table<string,string>
               for key, query in pairs(config or {}) do
-                if q == query and key:find('[%]%[][cC]') then
+                if q == query and key:find '[%]%[][cC]' then
                   vim.cmd('normal! ' .. key)
                   return
                 end
