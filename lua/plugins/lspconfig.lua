121d2d522762f68f965495e1848185070b28484f
lnb <lnbsa12345@gmail.com>
2025-07-28 15:21:28 +0530

    refactored lspconfig loop

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -23,38 +23,22 @@ return {
       },
     },
 
-    config = function(_, opts)
-      -- local ok_mason, mlsp = pcall(require, 'mason-lspconfig')
+    config = function()
       local all_servers = {
         'asm_lsp',
-        'lua_ls',
-        'clangd',
-        -- 'pyright',
         'basedpyright',
+        'clangd',
         'gopls',
+        'lua_ls',
+        -- 'pyright',
       }
-      -- if ok_mason then
-      --   all_servers = mlsp.get_installed_servers()
-      -- end
-
-      local servers = {}
-
-      local langdir = vim.fn.stdpath 'config' .. '/lua/lsp'
-      local langs = vim.fn.globpath(langdir, '*.lua', false, true)
-      for _, v in ipairs(langs) do
-        local s = vim.fn.fnamemodify(v, ':t:r')
-        table.insert(servers, s)
-      end
 
       for _, lang in ipairs(all_servers) do
-        local opt = {}
-        if vim.tbl_contains(servers, lang) then
-          opt = require('lsp.' .. lang)
-        else
-          opt.capabilities = require 'utils.lsp'.capabilities
-          opt.on_attach = require 'utils.lsp'.on_attach
-        end
-        require 'lspconfig'[lang].setup(opt)
+        local ok, opts = pcall(require, 'lsp.' .. lang)
+        opts = ok and opts or {}
+        opts.on_attach = opts.on_attach or require 'utils.lsp'.on_attach
+        opts.capabilities = opts.capabilities or require 'utils.lsp'.capabilities
+        require 'lspconfig'[lang].setup(opts)
       end
     end
   },
