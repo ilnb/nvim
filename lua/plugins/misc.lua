d92f2348a90dbcd0072245cb33ffb8209c4d5c6e
lnb <lnbsa12345@gmail.com>
2025-09-28 11:45:26 +0530

    no need for plenary now

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -310,7 +310,6 @@ return {
 
   {
     'jiaoshijie/undotree',
-    dependencies = 'nvim-lua/plenary.nvim',
     config = true,
     keys = {
       { '<leader>U', function() require 'undotree'.toggle() end, desc = 'Undotree' },
