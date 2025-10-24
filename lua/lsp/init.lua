3e09327f3af0e89486644865be3dfbb5fe1d8a3a
lnb <lnbsa12345@gmail.com>
2025-10-24 20:39:42 +0530

    added QoL keymaps

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -25,3 +25,106 @@ for server, ft in pairs(servers) do
     end
   })
 end
+
+vim.api.nvim_create_user_command('LspStop', function(opts)
+  local args = {}
+  if opts.args ~= "" then
+    for name in string.gmatch(opts.args, "%S+") do
+      table.insert(args, name)
+    end
+  end
+
+  local clients = vim.lsp.get_clients { bufnr = 0 }
+  if #clients == 0 then return end
+
+  for _, client in ipairs(clients) do
+    if args == {} or vim.tbl_contains(args, client.name) then
+      client:stop(true)
+    end
+  end
+end, {
+  nargs = '*',
+  complete = function()
+    local ret = {}
+    for server, _ in pairs(servers) do
+      ret[#ret + 1] = server
+    end
+    return ret
+  end
+})
+
+vim.api.nvim_create_user_command('LspStart', function(opts)
+  local args = {}
+  if opts.args ~= "" then
+    for name in string.gmatch(opts.args, "%S+") do
+      table.insert(args, name)
+    end
+  end
+  local ft = vim.bo.filetype
+
+  if #args == 0 then
+    for server, fts in pairs(servers) do
+      if vim.tbl_contains(fts, ft) then
+        local ok, cfg = pcall(require, 'lsp.' .. server)
+        cfg = ok and cfg or {}
+        cfg.on_attach = cfg.on_attach or require 'utils.lsp'.on_attach
+        cfg.capabilities = cfg.capabilities or require 'utils.lsp'.capabilities
+        cfg.name = server
+        cfg.root_markers = cfg.root_markers or { '.git' }
+        cfg.root_dir = require 'utils.plugins'.root_pattern(cfg.root_markers)(vim.api.nvim_buf_get_name(0))
+            or vim.fn.getcwd()
+
+        vim.lsp.start(cfg)
+      end
+    end
+  else
+    for _, server in ipairs(args) do
+      local fts = servers[server]
+      if fts and vim.tbl_contains(fts, ft) then
+        local ok, cfg = pcall(require, 'lsp.' .. server)
+        cfg = ok and cfg or {}
+        cfg.on_attach = cfg.on_attach or require 'utils.lsp'.on_attach
+        cfg.capabilities = cfg.capabilities or require 'utils.lsp'.capabilities
+        cfg.name = server
+        cfg.root_markers = cfg.root_markers or { '.git' }
+        cfg.root_dir = require 'utils.plugins'.root_pattern(cfg.root_markers)(vim.api.nvim_buf_get_name(0))
+            or vim.fn.getcwd()
+
+        vim.lsp.start(cfg)
+      end
+    end
+  end
+end, {
+  nargs = '*',
+  complete = function()
+    local ret = {}
+    for server, _ in pairs(servers) do
+      ret[#ret + 1] = server
+    end
+    return ret
+  end
+})
+
+vim.api.nvim_create_user_command('LspRestart', function()
+  local ft = vim.bo.filetype
+  local clients = vim.lsp.get_clients { bufnr = 0 }
+
+  for _, client in ipairs(clients) do
+    client:stop(true)
+  end
+
+  for server, fts in pairs(servers) do
+    if vim.tbl_contains(fts, ft) then
+      local ok, cfg = pcall(require, 'lsp.' .. server)
+      cfg = ok and cfg or {}
+      cfg.on_attach = cfg.on_attach or require 'utils.lsp'.on_attach
+      cfg.capabilities = cfg.capabilities or require 'utils.lsp'.capabilities
+      cfg.name = server
+      cfg.root_markers = cfg.root_markers or { '.git' }
+      cfg.root_dir = require('utils.plugins').root_pattern(cfg.root_markers)(vim.api.nvim_buf_get_name(0))
+          or vim.fn.getcwd()
+
+      vim.lsp.start(cfg)
+    end
+  end
+end, { nargs = 0 })
