b5daa653b0efc106dbc072e812cf41de62f65766
lnb <lnbsa12345@gmail.com>
2025-12-18 12:03:29 +0530

    added include path

--- lua/lsp/qmlls6.lua
+++ lua/lsp/qmlls6.lua
@@ -2,4 +2,9 @@ return {
   cmd = { 'qmlls6' },
   filetypes = { 'qml' },
   root_dir = require 'utils.plugins'.root_pattern '.git',
+  settings = {
+    QmlLsp = {
+      import_paths = { '/usr/lib/qt/qml' },
+    },
+  },
 }
