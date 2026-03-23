ce2fedaa0c81b63e442838cce3a8b63a3e25c6a3
lnb <lnbsa12345@gmail.com>
2026-03-23 20:46:08 +0530

    errors -_-

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -251,7 +251,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = { 'BufReadPre', 'BufNewFile' },
-    cmd = { "TSUpdate', 'TSInstall', 'TSUninstall', 'TSLog" },
     dependencies = {
       {
         'nvim-treesitter/nvim-treesitter-textobjects',
