838cd412605053c50dcec85fa691fef4763723c4
lnb <lnbsa12345@gmail.com>
2026-03-31 21:27:04 +0530

    add devicons dependency

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -463,6 +463,9 @@ return {
   {
     'nvim-mini/mini.icons',
     lazy = true,
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons', },
+    },
     opts = {
       file = {
         ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
