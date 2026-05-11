bbb1bd5fa3ac1597dda35a85a7c99a1225047017
lnb <lnbsa12345@gmail.com>
2026-05-11 23:58:23 +0530

    minor update

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -271,7 +271,7 @@ return {
       ensure = {
         'asm', 'bash', 'c', 'cpp', 'css', 'diff',
         'html', 'hyprlang', 'javascript', 'jsdoc',
-        'json', 'lua', 'luadoc', 'luap',
+        'json', 'lua', 'luadoc', 'luap', 'odin',
         'markdown', 'markdown_inline', 'python',
         'query', 'regex', 'toml', 'typescript',
         'typst', 'vim', 'vimdoc', 'xml', 'yaml',
